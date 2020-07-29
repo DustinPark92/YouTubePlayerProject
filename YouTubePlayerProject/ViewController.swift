c520caf2c315be8ca02252978b06eae943228538
Dustin <p4569zz@gmail.com>
2020-07-29 15:58:41 +0900

    Initial Commit

---
+++ YouTubePlayerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubePlayerProject
+//
+//  Created by Dustin on 2020/07/29.
+//  Copyright © 2020 Dustin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
